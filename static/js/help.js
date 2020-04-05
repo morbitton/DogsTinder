74dbcaa7e627166633cd08b7b4c1d526c3c0b308
mor <morbitton1993@gmail.com>
2020-04-06 00:15:32 +0300

    Website upload

---
+++ static/js/help.js
@@ -0,0 +1,17 @@
+		$(document).ready(function(){
+			$("#q1").click(function(){
+			$("#a1").toggle();
+		  });
+		  
+		  	$("#q2").click(function(){
+			$("#a2").toggle();
+		  });
+		  
+		  	$("#q3").click(function(){
+			$("#a3").toggle();
+		  });
+		  
+		  	$("#q4").click(function(){
+			$("#a4").toggle();
+		  });
+		});
